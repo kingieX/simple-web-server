042773edb5475548511cc4e50a2469b2e2a49db8
DeazStar <deadfromtheborn@gmail.com>
2023-03-13 15:12:59 -0400

    modify the function

--- extract_file_path.c
+++ extract_file_path.c
@@ -1,21 +1,18 @@
 #include "main.h"
 
 /**
- * extract the path of the requested file from the URL
- * @url: the url 
- * @path: the path of the requested file 
- * @size: the size of the path 
+ * Extract the path of the requested file from the URL and store it in file_req->path
+ * @file_req: a pointer to a file_request_t struct
  * 
  * Returns: Nothing
 */
-
-void extract_http_request_path(const char *url, char *path, size_t size)
+void extract_http_request_path(file_request_t *file_req)
 {
     const char *path_end;
-    const char *path_start = strstr(url, "://");
+    const char *path_start = strstr(file_req->path, "://");
     if (path_start == NULL) 
     {
-        path_start = url; 
+        path_start = file_req->path; 
     } 
     else 
     {
@@ -23,17 +20,17 @@ void extract_http_request_path(const char *url, char *path, size_t size)
         path_start = strchr(path_start, '/');
         if (path_start == NULL) 
         {
-            path_start = url + strlen(url);
+            path_start = file_req->path + strlen(file_req->path);
         }
     }
 
-    path_end = url + strlen(url);
+    path_end = file_req->path + strlen(file_req->path);
 
     size_t path_len = path_end - path_start;
-    if (path_len >= size) 
+    if (path_len >= sizeof(file_req->path)) 
     {
-        path_len = size - 1;  
+        path_len = sizeof(file_req->path) - 1;  
     }
-    strncpy(path, path_start, path_len);
-    path[path_len] = '\0';
+    strncpy(file_req->path, path_start, path_len);
+    file_req->path[path_len] = '\0';
 }
